e3d1bb452fa093750be5ab0ef16e7795a45e4028
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-12-05 22:42:20 +0800

    Add basic navigation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+/**
+ * charcoal: 404e5c
+ * payne's gray: 4f6272
+ * powder blue: a1b5d8
+ * baby powder: fffcf7
+ * nyanza: E4F0D0
+ */
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { goto } from '$app/navigation';
+
+  async function onClickHandler() {
+    let result = await goto("/notes");
+    return result
+  }
+</script>
+<h1>Noted</h1>
+<div>
+  <p>The note editor with markdown support.</p>
+  <p>Check out the back-end <a href="https://github.com/amrllkmn/noted"> here </a></p>
+  <button on:click={onClickHandler}>Try it out</button>
+</div>
