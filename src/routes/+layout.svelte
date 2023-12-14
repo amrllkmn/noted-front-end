3769c4f1e5bec53609dee7412ec5438d1df3dadf
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-12-14 23:26:38 +0800

    Add landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="bg-gradient-to-t from-powder-blue from-30% via-nyanza via-60% to-baby-powder to-90% h-screen flex flex-col items-center justify-between">
+  <slot />
+</main>
