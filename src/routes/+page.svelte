fe094cc7ee6dab9b5fdb9048ef41cc80f3c17f13
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2024-02-18 15:32:58 +0800

    Added more copy on the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,16 @@
 <title>Noted.</title>
 <div class="main">
 	<h1 class="title">Noted.</h1>
+	<p>A note-taking app, with Markdown support.</p>
 	<button class="navigation" on:click={() => navigateToPage('/notes')}>Try it out</button>
-	<p>
-		Check out <a href="https://kit.svelte.dev">the repo</a>, or go to
+	<p class="small">
+		Check out <a href="https://github.com/amrllkmn/noted">the back-end repo</a>,
+		<a href="https://github.com/amrllkmn/noted-front-end"> the front-end repo</a>
+		, or go to
 		<a href="https://codingchallenges.substack.com/p/coding-challenge-35-google-keep"
 			>Coding Challenges</a
 		>
-		to see what this app is based on.
+		to see what challenge this app is based on.
 	</p>
 </div>
 
@@ -25,15 +28,22 @@
 
 	p {
 		font-family: 'Space Grotesk';
-		font-weight: 300;
+		font-weight: 400;
 		font-size: 1rem;
 		line-height: 1.8rem;
+	}
+
+	p.small {
+		font-family: 'Space Grotesk';
+		font-weight: 300;
+		font-size: 0.8rem;
+		line-height: 0.8rem;
 	}
 
 	.main {
 		display: grid;
 		gap: 1rem;
-		grid-template-rows: 3fr 0fr 1fr;
+		grid-template-rows: 3fr 1fr 0fr 1fr;
 		height: 100vh;
 		justify-items: center;
 		align-content: center;
